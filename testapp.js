09eb69cb966e44ca90d99c0d9e98e1f335e57ae5
KraetSpg <man220274@spengergasse.at>
2025-02-23 19:07:08 +0100

    fixing 2

---
+++ testapp.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const willhaben = require('willhaben');
+
+let allowedOrigins = ['*']; // INSERT RIGHT DOMAIN HERE
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    willhaben.new()
+    .category(willhaben.getCategories["immobilien"])
+    .count(100)
+    .search().then(json => {
+        res.header("Access-Control-Allow-Origin", allowedOrigins);
+        res.send(json)
+    })
+})
