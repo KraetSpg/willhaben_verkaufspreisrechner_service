522793a5e6812f752011ce1c0dbed9b0d8a6962e
KraetSpg <man220274@spengergasse.at>
2025-02-23 18:43:33 +0100

    added json handler i guess

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const app = express()
 const port = process.env.PORT || 3000
 const willhaben = require('willhaben');
 
+let allowedOrigins = ['*']; // INSERT RIGHT DOMAIN HERE
+
+
 // Dashboard: https://app.koyeb.com/services/42a3e2b7-3496-45ca-b0c9-2e810d09bef0?deploymentId=db9c4347-0184-43e1-beeb-5b6aa65622f0
 // Deployment Link: https://independent-otha-kraetsoranisation-bb6455f7.koyeb.app/
 
-/*
-  - Since this got a bit complicated to my future self here is how it works:
-  Condition can be alle or specified
-  Searchtext can be none or specified
-  Category has to be specified
+app.use(express.json());
 
-  then im building a request with the willhaben module based on these factors
-
-  this has to be improved in the future
-*/
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/api/:category/:condition/:searchtext', (req, res) => {
-  let allowedOrigins = ['*']; // INSERT RIGHT DOMAIN HERE
 
   if (req.params.searchtext == "none") { // if no searchtext is entered
     if (req.params.condition == "alle") { // if no condition is selected
